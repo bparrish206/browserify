5f40c7dca2da962fb2737025fe814144161372b2
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-25 14:52:00 -0800

    simple browserify

---
+++ dateutils.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var logDate = function(){
+  console.log(new Date().getDate());
+};
+
+var logMonth = function() {
+  console.log(new Date().getMonth());
+};
+
+exports.logDate = logDate;
+exports.logMonth = logMonth;
